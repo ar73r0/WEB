bf2ecbc1d2cba8f02d5fc3c1ca0588d0101205ca
Aaron S <aaron.sengier@gmail.com>
2024-03-20 10:37:55 +0100

    trying form validation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-'use strict'
+'use strict';
+const home = 'home'
 
+document.getElementById('home').innerText = home; //Elementen selecteren + Elementen manipuleren
+document.getElementById('home').addEventListener('mouseover', e => { alert('watch out!');});//Event aan een element koppelen
+
+validate = (name) => {
+    alert('thank you', name);
+};
